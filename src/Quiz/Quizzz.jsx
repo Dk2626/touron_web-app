24c359360b3be747a00f456cdd5f3804c3bba3ac
Dk2626 <dineshkumardssd@gmail.com>
2022-10-06 11:07:23 +0530

    video added in lucky

--- src/Quiz/Quizzz.jsx
+++ src/Quiz/Quizzz.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import luckyseatvideo from '../assests/Quiz/video/luckyseatvideo.mp4';
-import ReactPlayer from 'react-player';
+// import ReactPlayer from 'react-player';
 
 const Quizzz = () => {
   return (
     <div>
-      <ReactPlayer url={luckyseatvideo} width='50%' height='50%' controls />
+      <div>
+        {/* <ReactPlayer url={luckyseatvideo} width='50%' height='50%' controls /> */}
+        <video
+          src={luckyseatvideo}
+          width='50%'
+          height='50%'
+          autoPlay
+          muted
+          loop
+          playsInline
+        />
+      </div>
+      <div>
+        <h1>Video</h1>
+      </div>
     </div>
   );
 };
