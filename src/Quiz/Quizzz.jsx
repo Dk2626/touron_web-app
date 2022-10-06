08c954af1129ae1eb862067e0461b09d366b346f
Dk2626 <dineshkumardssd@gmail.com>
2022-10-06 14:40:22 +0530

    vi modified

--- src/Quiz/Quizzz.jsx
+++ src/Quiz/Quizzz.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import luckyseatvideo from '../assests/Quiz/video/luckyseatvideo.mp4';
-import luckyseatvideo1 from '../assests/Quiz/video/hero1.mp4';
-// import ReactPlayer from 'react-player';
 
 const Quizzz = () => {
+  const videoRef = useRef(undefined);
+  useEffect(() => {
+    videoRef.current.defaultMuted = true;
+    console.log('sd');
+  });
+
   return (
     <div>
       <div>
-        {/* <ReactPlayer url={luckyseatvideo} width='50%' height='50%' controls /> */}
         <video
-          src={luckyseatvideo1}
-          width='50%'
-          height='50%'
+          style={{ width: '50%', height: '50%' }}
+          ref={videoRef}
+          loop
           autoPlay
           muted
-          loop
-          playsInline
-          controls
-        />
+          playsInline>
+          <source src={luckyseatvideo} type='video/mp4' />
+        </video>
+      </div>
+      <div>
+        <h1>Video</h1>
       </div>
       <div>
         <h1>Video</h1>
